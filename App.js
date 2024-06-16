c6f01184cd77ba505ee1a4de3aebaa4cdee19a5a
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2024-06-16 18:11:30 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/home';
+import Login from './screens/login';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ screens/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Login = ({navigation}) => {
+    return (
+        <View>
+            <Text>Login</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Login ;
